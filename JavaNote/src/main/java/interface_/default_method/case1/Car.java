2d0ba824835cc63ec4bd27256bd6cd65f29803d8
Duy <duyduyly123vn@gmail.com>
2025-03-20 22:06:55 +0700

    java note | Interface

---
+++ JavaNote/src/main/java/interface_/default_method/case1/Car.java
@@ -0,0 +1,23 @@
+package interface_.default_method.case1;
+
+interface Vehicle {
+    void start();  // Abstract method (must be implemented)
+
+    default void stop() {  // Default method with implementation
+        System.out.println("Vehicle is stopping...");
+    }
+}
+
+class Car implements Vehicle {
+    public void start() {
+        System.out.println("Car is starting...");
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        myCar.start();
+        myCar.stop();  // Calls the default method from Vehicle
+    }
+}
