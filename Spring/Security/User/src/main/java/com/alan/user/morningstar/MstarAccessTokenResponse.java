0e7dd362decb9e502f045e80b7ec8d12593d5476
Duy <duyduyly123vn@gmail.com>
2025-04-23 15:05:18 +0700

    java note | Change Project Name From Java Note to JavaCore

---
+++ Spring/Security/User/src/main/java/com/alan/user/morningstar/MstarAccessTokenResponse.java
@@ -0,0 +1,27 @@
+package com.alan.user.morningstar;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MstarAccessTokenResponse {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("expires_in")
+    private Long expiresIn;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+}
