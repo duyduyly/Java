b9ef797d0fef99f2909a748783ae63cf993e15fb
DuyLy <duyduyly123vn@gmail.com>
2025-06-09 09:22:59 +0700

    java-note | Add Temp Exception Doc

--- JavaCore/src/main/java/App.java
+++ JavaCore/src/main/java/App.java
@@ -1,11 +1,35 @@
 import clazz.beyond_clazz.enums.Compass2;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Random;
 
 public class App {
     public static void main(String[] args) {
 
         System.out.println(Compass2.SOUTH.getDirection());
+        try {
+            int[] a = {-7,11,3};
+            printFourthElement(a);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void printFourthElement(int[] a){
+        System.out.println(a[3]);
+    }
+    void readFirstByteFromFile(File fileName){
+        FileInputStream file = null;
+        try {
+            file = new FileInputStream(fileName);
+            byte x = (byte) file.read();
+            System.out.println(x);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
--- JavaCore/src/main/java/exception/App.java
+++ JavaCore/src/main/java/exception/App.java
@@ -1,13 +1,69 @@
 package exception;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public class App {
     public static ExceptionUtils eu = new ExceptionUtils();
 
     public static void main(String[] args) {
-        Optional<String> ab = Optional.empty();
-        String demoThrowException = eu.findOrThrow(ab, new RuntimeException("Demo Throw Exception"));
-        System.out.println(demoThrowException);
+//        Optional<String> ab = Optional.empty();
+//        String demoThrowException = eu.findOrThrow(ab, new RuntimeException("Demo Throw Exception"));
+//        System.out.println(demoThrowException);
+
+        // Usage:
+        runWithCatch("sdsdsd", true, () -> {
+            System.out.println("try body block here~!");
+            System.out.println(2 / 0);
+        });
+    }
+
+    public static void runWithCatch(String message, boolean canThrow, CheckedRunnable runnable) {
+        try {
+            runnable.run();
+        } catch (Exception e) {
+            if (canThrow) {
+                throw new RuntimeException(message, e);
+            }
+            // handle once here
+            System.err.println(e.getMessage());
+        }
     }
 }
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+class ErrorResponse {
+    private int status;
+    private String message;
+    private LocalDateTime timestamp = LocalDateTime.now();
+
+    public static ErrorResponse ErrorResponse(String message) {
+        return ErrorResponse.builder().status(500).timestamp(LocalDateTime.now()).message(message).build();
+    }
+
+    public static String ErrorResponseJson(String message) throws JsonProcessingException {
+        return new ObjectMapper().writeValueAsString(ErrorResponse(message));
+    }
+}
+
+@FunctionalInterface
+interface CheckedRunnable {
+    void run() throws Exception;
+}
+
+
+
+
+
